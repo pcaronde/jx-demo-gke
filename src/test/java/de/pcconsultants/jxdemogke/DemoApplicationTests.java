81a2f9c4a19777d839822f21b35620f1a96eca24
Peter Caron <pcaron.de@gmail.com>
2019-01-20 13:28:11 +0100

    Comments in Test

--- src/test/java/de/pcconsultants/jxdemogke/DemoApplicationTests.java
+++ src/test/java/de/pcconsultants/jxdemogke/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package de.pcconsultants.jxdemogke;
 
+// Add some superfulous comments
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
